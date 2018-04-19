18e523b2146b6bbdece257deaa07efaef1dd26e3
Justus Fowl <justusfowl@gmail.com>
2018-04-19 18:35:15 +0200

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,16 +2,16 @@ export const environment = {
   production: true,
   ENV :{
     mode : "prod", 
-    hostURL : 'comfash.local', 
-    hostPort: "9999", 
+    hostURL : 'comfash.com', 
+    hostPort: "443", 
     apiVersion : "01", 
     auth : {
       clientID: 'IEFi9KoFw0QdQbtoXoCcD523MZ3OVULr',
       domain: 'comfash.eu.auth0.com',
       responseType: 'token id_token',
       audience: 'https://comfash.eu.auth0.com/api/v2/',
-      redirectUri: 'https://comfash.local:9999/',
-      scope: 'openid profile email' 
+      redirectUri: 'https://comfash.com/',
+      scope: 'openid profile email app_metadata' 
     }
   }
 };
